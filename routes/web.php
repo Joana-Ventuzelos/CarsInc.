66a9790b43292fc59ddf91937eb1a3039b1bc539
Joana <jr_ventuzelos@hotmail.com>
2025-05-16 14:52:45 +0100

    Add reservation confirmation email functionality and UI

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReservationConfirmationMailController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,5 +17,9 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::post('/enviar-email', [ReservationConfirmationMailController::class, 'sendReservationEmail'])
+    ->middleware('auth')
+    ->name('send.email');
 
 require __DIR__.'/auth.php';
