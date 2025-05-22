923e8ca872c8c8adb397e700f1f60663c35e524b
Joana <jr_ventuzelos@hotmail.com>
2025-05-22 10:31:48 +0100

    add nif

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -16,6 +16,13 @@
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
+        <!-- NIF -->
+        <div class="mt-4">
+            <x-input-label for="nif" :value="__('NIF')" />
+            <x-text-input id="nif" class="block mt-1 w-full" type="text" name="nif" :value="old('nif')" required autocomplete="nif" />
+            <x-input-error :messages="$errors->get('nif')" class="mt-2" />
+        </div>
+
         <!-- Password -->
         <div class="mt-4">
             <x-input-label for="password" :value="__('Password')" />
