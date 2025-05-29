f010e648579917db93592abcbc6c293413eee4e2
Joana <jr_ventuzelos@hotmail.com>
2025-05-29 20:41:13 +0100

    N/a TO PAYPAL

--- resources/views/reservation/history.blade.php
+++ resources/views/reservation/history.blade.php
@@ -57,7 +57,7 @@
                                         @if($rental->payments && $rental->payments->isNotEmpty())
                                             {{ ucfirst($rental->payments->first()->payment_method) }}
                                         @else
-                                            N/A
+                                            PayPal
                                         @endif
                                     </td>
                                     <td class="py-2 px-4 border-b border-gray-200 dark:border-gray-700">
