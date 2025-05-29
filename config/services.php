7a883858bad57379413fe6a86369c813a6078c5b
Joana <jr_ventuzelos@hotmail.com>
2025-05-29 09:18:46 +0100

    pay

--- config/services.php
+++ config/services.php
@@ -25,10 +25,12 @@ return [
     ],
 
     'paypal' => [
-    'client_id' => env('PAYPAL_CLIENT_ID'),
-    'secret' => env('PAYPAL_SECRET'),
+    'client_id' => env('PAYPAL_SANDBOX_CLIENT_ID'),
+    'secret' => env('PAYPAL_SANDBOX_CLIENT_SECRET'),
     'mode' => env('PAYPAL_MODE', 'sandbox'),
 ],
+
+
     'resend' => [
         'key' => env('RESEND_KEY'),
     ],
@@ -40,10 +42,6 @@ return [
         ],
     ],
 
-    'paypal' => [
-        'client_id' => env('PAYPAL_CLIENT_ID'),
-        'secret' => env('PAYPAL_SECRET'),
-        'mode' => env('PAYPAL_MODE', 'sandbox'),
-    ],
+
 
 ];
