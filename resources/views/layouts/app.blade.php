06bf42388e81ca16474371e9baefece6ff38134d
Joana <jr_ventuzelos@hotmail.com>
2025-05-30 15:22:46 +0100

    final

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -59,7 +59,7 @@
                 </div>
             </div>
             <div class="text-center text-xs text-gray-400 py-2 border-t border-gray-800 mt-4">
-                &copy; {{ date('Y') }} Cars 2.0. Todos os direitos reservados e um especial agradecimento ao Luís Mago, Ângela Peixoto e Rui Silva.
+                &copy; {{ date('Y') }} Cars 2.0. Todos os direitos reservados a Joana Ventuzelos e um especial agradecimento ao Luís Mago, Ângela Peixoto e Rui Silva.
             </div>
         </footer>
     </body>
