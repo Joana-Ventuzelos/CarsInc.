fd82f1460baa4a717ce89fccd8805679f165cacd
Joana <jr_ventuzelos@hotmail.com>
2025-06-02 15:49:54 +0100

    change of names

--- resources/views/finish-transaction.blade.php
+++ resources/views/finish-transaction.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PayPal Checkout - Laravel</title>
+    <title>ATM Checkout - Laravel</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
